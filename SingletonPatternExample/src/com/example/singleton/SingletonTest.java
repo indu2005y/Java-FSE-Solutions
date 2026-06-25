27b171e100d9cdc023f22667d5ec094ef88c6e82
y.indu <yakaindu@gmail.com>
2026-06-25 15:39:47 +0530

    Added SingletonPatternExample

---
+++ SingletonPatternExample/src/com/example/singleton/SingletonTest.java
@@ -0,0 +1,15 @@
+package com.example.singleton;
+
+public class SingletonTest {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application Started");
+        logger2.log("Application Running");
+
+        System.out.println(logger1 == logger2);
+    }
+}
