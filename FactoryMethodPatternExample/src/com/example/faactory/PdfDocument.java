57ad4f567ea15d0e6b6d54ba3c3fd7114c6c4b5d
y.indu <yakaindu@gmail.com>
2026-06-25 15:56:05 +0530

    added FactoryMethodPatternExample

---
+++ FactoryMethodPatternExample/src/com/example/faactory/PdfDocument.java
@@ -0,0 +1,9 @@
+package com.example.faactory;
+
+public class PdfDocument implements Document {
+
+    @Override
+    public void open() {
+        System.out.println("PDF Document Opened");
+    }
+}
