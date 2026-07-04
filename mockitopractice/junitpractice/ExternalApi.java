53b40186a102e04030f364d0e85ce5c1ea59861b
y.indu <yakaindu@gmail.com>
2026-07-04 21:14:24 +0530

    Add files via upload
    Added to mockitopractice

---
+++ mockitopractice/junitpractice/ExternalApi.java
@@ -0,0 +1,5 @@
+package com.junitpractice;
+
+public interface ExternalApi {
+    String getData();
+}
---
+++ mockitopractice/junitpractice/MyServiceTest.java
@@ -0,0 +1,20 @@
+package com.junitpractice;
+
+import static org.mockito.Mockito.*;
+
+import org.junit.Test;
+
+public class MyServiceTest {
+
+    @Test
+    public void testVerifyInteraction() {
+
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        MyService service = new MyService(mockApi);
+
+        service.fetchData();
+
+        verify(mockApi).getData();
+    }
+}
