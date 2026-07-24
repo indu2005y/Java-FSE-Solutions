9a42dd225bca93c3dcfc14290f13b1514d99d54c
y.indu <yakaindu@gmail.com>
2026-07-24 14:17:46 +0530

    Added to Ex11

---
+++ React/Ex11/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import EventExamples from "./components/EventExamples";
+import CurrencyConvertor from "./components/CurrencyConvertor";
+
+function App() {
+
+  return (
+    <div>
+
+      <h1>React Event Examples</h1>
+
+      <EventExamples />
+
+      <hr />
+
+      <CurrencyConvertor />
+
+    </div>
+  );
+
+}
+
+export default App;
---
+++ React/Ex11/EventExamples.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+
+class EventExamples extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      count: 0
+    };
+  }
+
+  increment = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+
+  decrement = () => {
+    this.setState({
+      count: this.state.count - 1
+    });
+  };
+
+  sayHello = () => {
+    alert("Hello! This is Static Message");
+  };
+
+  handleIncrease = () => {
+    this.increment();
+    this.sayHello();
+  };
+
+  sayWelcome = (msg) => {
+    alert(msg);
+  };
+
+  onPress = () => {
+    alert("I was clicked");
+  };
+
+  render() {
+    return (
+      <div>
+
+        <h2>Counter : {this.state.count}</h2>
+
+        <button onClick={this.handleIncrease}>
+          Increment
+        </button>
+
+        <button onClick={this.decrement}>
+          Decrement
+        </button>
+
+        <br /><br />
+
+        <button onClick={() => this.sayWelcome("Welcome")}>
+          Say Welcome
+        </button>
+
+        <br /><br />
+
+        <button onClick={this.onPress}>
+          OnPress
+        </button>
+
+      </div>
+    );
+  }
+}
+
+export default EventExamples;
