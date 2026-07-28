6b553580248bc3d57e36db7209d6c89ec4b23c1c
Indu <yakaindu@gmail.com>
2026-07-28 10:21:12 +0530

    Completed Angular EX4 - Template Driven Forms and Validation

---
+++ angular/EX4/student-course-portal/src/app/app.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+import { Header } from './components/header/header';
+import { Home } from './pages/home/home';
+import { CourseList } from './pages/course-list/course-list';
+import { EnrollmentForm } from './pages/enrollment-form/enrollment-form';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    RouterOutlet,
+    FormsModule,
+    Header,
+    Home,
+    CourseList,
+    EnrollmentForm
+  ],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  title = 'student-course-portal';
+}
